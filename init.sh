3580b018094461f39ed9d4f395510126827efbbe
c2v4 <the_pro@wp.pl>
2019-10-27 22:32:33 +0100

    Changes

--- init.sh
+++ init.sh
@@ -5,14 +5,8 @@ apt-get update -y
 apt-get upgrade -y
 
 echo -n "Installing Xubuntu and Ubuntu session"
-apt-get install -y xfce4
-
-  # Optional: Use LightDM login screen (-> not required to run "startx")
-  apt-get install -y lightdm lightdm-gtk-greeter
-  # Optional: Install a more feature-rich applications menu
-  apt-get install -y xfce4-whiskermenu-plugin
-
-sed -i 's/allowed_users=.*$/allowed_users=anybody/' /etc/X11/Xwrapper.config
+apt-get install -y xubuntu-desktop
+apt-get install -y ubuntu-session
 
 echo -n "Installing virtualbox guest additions"
 apt-get install -y virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11 linux-headers-$(uname -r)
@@ -22,15 +16,17 @@ apt-get install -y default-jdk
 
 echo -n "Installing Intellij"
 apt-get install -y snapd
-snap install intellij-idea-ultimate --classic
+#snap install intellij-idea-ultimate --classic
 
 echo -n "Installing tooling"
 apt-get install -y tilix yadm fasd git
 
 echo -n "Installing ZSH"
 apt-get install -y zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/antigen.git
-chown -R vagrant:vagrant antigen
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#git clone https://github.com/zsh-users/antigen.git
+#chown -R vagrant:vagrant antigen
+chsh -s $(which zsh) vagrant
 
-#yadm clone https://github.com/c2v4/dotfiles
+su -c 'ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa' vagrant
+su -c 'yadm clone https://github.com/c2v4/dotfiles' vagrant

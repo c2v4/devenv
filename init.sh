e04b772a96c05617ca76705a33b61136a677be40
Mateusz Duda <c2v4@users.noreply.github.com>
2019-11-15 08:26:16 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -23,7 +23,7 @@ snap install node --channel=13/stable --classic
 
 echo -n "Installing Docker and Kubernetes"
 apt-get install -y docker docker-compose
-sudo snap install microk8s --classic
+#sudo snap install microk8s --classic
 
 echo -n "Installing tooling"
 apt-get install -y tilix yadm fasd git maven

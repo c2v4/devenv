60825d598db5c4f67d804cc827e9fd56eb7a47d3
Mateusz Duda <c2v4@users.noreply.github.com>
2020-01-20 22:33:38 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-vagrant up --provider=VirtualBox
-vagrant package --output devenv.box --provider=VirtualBox
+vagrant up --provider=virtualbox
+vagrant package --output devenv.box --provider=virtualbox
 vagrant cloud auth login --token $1
 vagrant cloud publish c2v4/devenv 1.0.0 virtualbox devenv.box  --force --release

7f41d0035b45ecc6ac5791dea5a626600172bfa0
Mateusz Duda <the_pro@wp.pl>
2020-02-23 15:24:46 +0100

    Added installing provisioner to the buildscript

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-vagrant up --provider=virtualbox
+vagrant up --provider=virtualbox --instal-provider
 vagrant package --output devenv.box --provider=virtualbox
 vagrant cloud auth login --token $1
 vagrant cloud publish c2v4/devenv 1.0.0 virtualbox devenv.box  --force --release
